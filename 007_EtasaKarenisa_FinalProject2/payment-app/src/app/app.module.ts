42e16fda68e6ea93a0421b328e43aca266a29746
Etasa K <ekarenisa@yahoo.co.id>
2021-11-04 00:36:54 +0700

    first commit

---
+++ 007_EtasaKarenisa_FinalProject2/payment-app/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ReadComponent } from './components/read/read.component';
+import { CreateComponent } from './components/create/create.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { httpInterceptorProviders } from './interceptors';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ReadComponent,
+    CreateComponent,
+    UpdateComponent,
+    DeleteComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [httpInterceptorProviders,],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
